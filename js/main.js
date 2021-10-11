a57101ec421c1daf6c37178435842c35fb9b4180
AnnaPomogaeva <avpomogaeva@avito.ru>
2021-10-11 23:26:26 +0300

    Фикс ошибок проверки ESLint.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@ function getRandomInRange(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+getRandomInRange(0, 5);
 
 function checkComentaryLength(commentary, maxLen) {
   return commentary.length <= maxLen;
 }
+checkComentaryLength('ПРЕВЕД, МЕДВЕД!', 50);
